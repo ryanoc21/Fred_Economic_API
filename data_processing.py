5d74daa56ebda162d3b399299a16c17a4e2fb2ce
ryanoconnor <ryan19.roc@gmail.com>
2023-02-03 10:39:29 +0000

    Initial Commit

---
+++ data_processing.py
@@ -0,0 +1,4 @@
+"""
+This file contains the code for the clas that will perform the backend functionality of the project. 
+It contains methods that will read data from the API and plot time series graphs using plotly. 
+"""
